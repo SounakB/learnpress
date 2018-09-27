bdd89be343009fca5390a051fe1c74e01c9cb634
Nick Williams <nick.williams@striderhq.local>
2018-09-27 10:25:36 -0600

    fix shortcode

--- inc/api/class-lp-api.php
+++ inc/api/class-lp-api.php
@@ -86,7 +86,7 @@ class LP_API{
     $certified = array();
     foreach($users as $u) {
       $certCourses = $this->get_certification_courses_passed($u->ID);
-      $bgStatus = get_user_meta($uID, 'user_bg_check_passed', true);
+      $bgStatus = get_user_meta($u->ID, 'user_bg_check_passed', true);
       if(sizeof($certCourses) > 0 && $bgStatus != false){
         $user_name = $u->user_firstname .'_'. $u->user_lastname .'_'. $u->user_email;
         $certified[$user_name] = $certCourses;
